8b2b9ed519e5d41c8da07a7f50911fd85d2bbd50
ofry <tim4job@bmail.ru>
2020-07-26 05:19:39 +0300

    Without this commit, any library builds will fail - MinGW LD don't support -Bforcearchive.
    Signed-off-by: ofry <tim4job@bmail.ru>

--- mk/sys/MINGW32.mk
+++ mk/sys/MINGW32.mk
@@ -1,4 +1,4 @@
-#	$Id: Linux.mk,v 1.9 2017/05/05 18:02:16 sjg Exp $
+#	$Id: MINGW32.mk,v 1.9 2017/05/05 18:02:16 sjg Exp $
 #	$NetBSD: sys.mk,v 1.19.2.1 1994/07/26 19:58:31 cgd Exp $
 #	@(#)sys.mk	5.11 (Berkeley) 3/13/91
 
@@ -14,11 +14,18 @@ NEED_SOLINKS=yes
 
 .SUFFIXES: .out .a .ln .o .c ${CXX_SUFFIXES} .F .f .r .y .l .s .S .cl .p .h .sh .m4
 
-.LIBS:		.a
+.LIBS:		.a .dll
+
+SHLIBEXT =	.dll
+HOST_LIBEXT =	.dll
+DSHLIBEXT =	.dll
 
 AR=		ar
 ARFLAGS=	rl
 RANLIB=		ranlib
+LD_shared= -Bshareable
+LD_so= dll.${SHLIB_FULLVERSION}
+LD_solink= dll
 
 AS=		as
 AFLAGS=
